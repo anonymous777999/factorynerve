92030ab2617c3821d6af1141e6c931e1075d0265
anonymous777999 <anonymous152023@gmail.com>
2026-06-21 12:45:50 +0530

    chore: alembic merge migration for steel_fraud and entry_quality branches

---
+++ alembic/versions/b9f67568e281_merge_steel_fraud_and_entry_quality.py
@@ -0,0 +1,23 @@
+"""merge_steel_fraud_and_entry_quality
+
+Revision ID: b9f67568e281
+Revises: 20260619_07, 20260621_01
+Create Date: 2026-06-21 12:45:24.388065
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9f67568e281'
+down_revision = ('20260619_07', '20260621_01')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
